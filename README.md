b8ab3dd6193b2f7c08850702bf7f54d6de13f0b8
root <root@sora.sora>
2025-01-22 21:10:01 +0700

    [src]initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wnexto
-Wrapped NexTo Coin
+## Project Source
+
+Source code files for The NexTo Coin token's smart contract can be found in the `contracts` subdirectory. Contracts are built / deployed using the Truffle suite; compiled using solc version 0.8.7.
+
+This project's Truffle suite configuration can be found in the `truffle-config.js` file; the Node package configuration is set within the `package.json` file. Package scripts to call common truffle commands are setup:
+```
+$ npm run compile       # Compile Smart Contracts
+$ npm run migrate       # Deploy Smart Contracts
+$ npm run verify        # Verify Deployment on BSCScan
+```
+___
+
+## The Wrapped NexTo Project
+Wrapped NexTo was created with the belief that there need to be stronger, more trustworthy, and more worthwhile projects built throughout the deFi space. We decided to take this into our own hands by launching Wrapped NexTo; a BEP-20 token featuring battle tested tokenomics, an innovative future roadmap, and a trustworthy team that cares for its investors.
+
+## Specification
---
+++ migrations/1_nextocoin_migration.js
@@ -0,0 +1,76 @@
+const Migrations = artifacts.require("Migrations");
+const NexToToken = artifacts.require("NexToToken");
+const IPancakeRouter02 = artifacts.require("IPancakeRouter02");
+
+const TestnetPancakeRouter = '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3';
+const MainnetPancakeRouter = '0x10ED43C718714eb63d5aA57B78B54704E256024E';
+
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Migrations);
+
+  let NexToToken, currentTx;
+  if(network == 'testnet'){
+    NexToToken = await deployer.deploy(NexToToken, TestnetPancakeRouter);
+
+    console.log("\n   Configuring 'NexToToken'");
+    console.log("   ---------------------------");
+
+    currentTx = await NexToToken.addManagementFeesReciever('0x1A1D9d3581e0aE2569349d3574327bfb77aC2B4B');
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.addManagementFeesReciever('0x1A1D9d3581e0aE2569349d3574327bfb77aC2B4B');
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setMinPendingFeesForAutoLiquify(BigInt(750000000));
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setMinReserveETHForAutoBuyback(BigInt(1000000000000));
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setMinReserveETHForAutoReinject(BigInt(110000000000000000));
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    console.log("\n   Configuring 'PancakeRouter'");
+    console.log("   ---------------------------");
+
+    var pancakeRouter = await IPancakeRouter02.at(TestnetPancakeRouter);
+
+    currentTx = await NexToToken.approve(pancakeRouter.address, BigInt(90000000000000));
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await pancakeRouter.addLiquidityETH(NexToToken.address, 90000000000000, 0, 0, NexToToken.address, Math.floor(Date.now() / 1000) + 300, {value: 1000000000000000000});
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    console.log("\n   Configuring 'NexToToken'");
+    console.log("   ---------------------------");
+
+    currentTx = await NexToToken.setLimitsEnabled(true);
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setFeesEnabled(true);
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setAutoFeeLiquifyEnabled(true);
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setAutoBuybackEnabled(true);
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.setAutoReinjectEnabled(true);
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+  } else {
+    NexToToken = await deployer.deploy(NexToToken, MainnetPancakeRouter);
+    
+    console.log("\n   Configuring 'NexToToken'");
+    console.log("   ---------------------------");
+
+    currentTx = await NexToToken.addManagementFeesReciever('0x1A1D9d3581e0aE2569349d3574327bfb77aC2B4B');
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+
+    currentTx = await NexToToken.addManagementFeesReciever('0x1A1D9d3581e0aE2569349d3574327bfb77aC2B4B');
+    console.log(`   > transaction hash: ${currentTx.tx}`);
+  }
+  currentTx = await NexToToken.sendTransaction({from: accounts[0], value: 100000000000000000});
+  console.log(`   > transaction hash: ${currentTx.tx}`);
+
+};
